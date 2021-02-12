3b766755c2fa696ba58f04929c25deebefab1ec5
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-02-12 14:29:05 +0300

    First Commit

---
+++ DentistCalendar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DentistCalendar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
