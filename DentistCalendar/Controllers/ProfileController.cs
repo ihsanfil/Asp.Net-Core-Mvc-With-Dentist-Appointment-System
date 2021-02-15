b81f22e9803b4c7537048c75dc36081f799ba5e6
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-02-15 17:59:13 +0300

    sekreter sayfası ve modelleri olusturuldu

---
+++ DentistCalendar/Controllers/ProfileController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DentistCalendar.Data.Entity;
+using DentistCalendar.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentistCalendar.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+        public ProfileController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            AppUser user = _userManager.Users.SingleOrDefault(x => x.UserName == HttpContext.User.Identity.Name); //oturum açmış olan kullanıcının adını alıyoruz
+            if (user == null)
+            {
+                return View("Error");
+            }
+            if (_userManager.IsInRoleAsync(user,"Secretary").Result)
+            {
+                SecretaryViewModel model = new SecretaryViewModel()
+                {
+                    User = user,
+                    Dentists = _userManager.Users.Where(x => x.IsDentist)
+                };
+                return View("Secretary",model);
+            }
+            else
+            {
+                return View("Dentist");
+            }
+        }
+        public IActionResult Secretary()
+        {
+            return View();
+        }
+        public IActionResult Dentist()
+        {
+            return View();
+        }
+    }
+}
---
+++ DentistCalendar/Models/SecretaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DentistCalendar.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DentistCalendar.Models
+{
+    public class SecretaryViewModel
+    {
+        public AppUser User { get; set; }
+        public IEnumerable<AppUser> Dentists { get; set; }
+    }
+}
